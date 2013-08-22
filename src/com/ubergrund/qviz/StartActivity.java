5e13ec1c37a6f4e0cce78646df8b8e31df1a5d48
Tim <tim@ubergrund.de>
2013-08-22 21:02:46 +0200

    initial commit

---
+++ src/com/ubergrund/qviz/StartActivity.java
@@ -0,0 +1,19 @@
+package com.ubergrund.qviz;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tim
+ * Date: 8/22/13
+ * Time: 6:28 PM
+ */
+public class StartActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startService(new Intent(this, QVizService.class));
+    }
+}
